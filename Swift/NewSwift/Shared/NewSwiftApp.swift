248e79a45b19674112d46c18b5a4707d5d67d9fc
MrSui1994 <249184521@qq.com>
2021-03-24 10:38:14 +0800

    提交NewSwift项目

---
+++ Swift/NewSwift/Shared/NewSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSwiftApp.swift
+//  Shared
+//
+//  Created by MrSui on 2021/3/24.
+//
+
+import SwiftUI
+
+@main
+struct NewSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
